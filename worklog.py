31af3ad2ac3b86252a81096edd14ceca3b3329b5
Alan W. Smith <alan@alanwsmith.com>
2017-02-10 10:56:33 -0500

    Alphabetized methods.

--- worklog.py
+++ worklog.py
@@ -5,38 +5,11 @@ import re
 
 class Worklog:
 
-    def main_prompt(self):
-        """Return the main prompt value
-        
-        >>> wl = Worklog()
-        >>> wl.main_prompt()
-        '1 = Add\\n2 = Lookup'
-        """
-        return("1 = Add\n2 = Lookup")
-
     def ask_for_input(self):
         """Generic method to gather user input to
         pass on to other methods for validaiton.
         """
         input("> ")
-
-    def validate_main_prompt_input(self, test_string):
-        """Make sure a value of '1' or '2' was passed
-
-        >>> wl = Worklog()
-        >>> wl.validate_main_prompt_input("1")
-        True
-        >>> wl.validate_main_prompt_input("2")
-        True
-        >>> wl.validate_main_prompt_input("asdfasdf")
-        False
-
-        """
-        pattern = re.compile("^(1|2)$")
-        if pattern.match(test_string):
-            return True 
-        else:
-            return False
 
     def get_new_entry_data(self):
         print("What is your name?")
@@ -60,6 +33,32 @@ class Worklog:
         """
         return "How do you want to find previous entries?\n1 = By Employee\n2 = By Date\n3 = By Search Term"
         
+    def main_prompt(self):
+        """Return the main prompt value
+        
+        >>> wl = Worklog()
+        >>> wl.main_prompt()
+        '1 = Add\\n2 = Lookup'
+        """
+        return("1 = Add\n2 = Lookup")
+
+    def validate_main_prompt_input(self, test_string):
+        """Make sure a value of '1' or '2' was passed
+
+        >>> wl = Worklog()
+        >>> wl.validate_main_prompt_input("1")
+        True
+        >>> wl.validate_main_prompt_input("2")
+        True
+        >>> wl.validate_main_prompt_input("asdfasdf")
+        False
+
+        """
+        pattern = re.compile("^(1|2)$")
+        if pattern.match(test_string):
+            return True 
+        else:
+            return False
 
 
 
