95f53ff4e48e007afffebb3868a82e302de86086
Alan W. Smith <alan@alanwsmith.com>
2017-02-16 12:11:10 -0500

    Added TODO list.

--- worklog.py
+++ worklog.py
@@ -382,10 +382,20 @@ if __name__ == "__main__":
             wl.add_task({"employee": employee, "task": task, "minutes": minutes, "notes": notes, "date": strftime("%Y-%m-%d", gmtime()) })
 
         elif check_input == "2":
+            
             print("Looking up")
         else:
             print("Quitting")
 
+"""
+[x] - As a user of the script, I should be able to choose whether to add a new entry or lookup previous entries.
+[x] - As a user of the script, if I choose to enter a new work log, I should be able to provide my name, a task name, a number of minutes spent working on it, and any additional notes I want to record.
+[] - As a user of the script, if I choose to find a previous entry, I should be presented with three options: find by employee, find by date, find by search term.
+[] - As a user of the script, if finding by date, I should be presented with a list of dates with entries and be able to choose one to see entries from.
+[] - As a user of the script, if finding by a search term, I should be allowed to enter a string and then be presented with entries containing that string in the task name or notes.
+[] - As a user of the script, if finding by employee, I should be allowed to enter employee name and then be presented with entries with that employee as their creator.
+[] - As a fellow developer, I should find at least 50% of the code covered by tests. I would use coverage.py to validate this amount of coverage.
+"""
 
     # TODO: Test everything with an empty database too.
 
